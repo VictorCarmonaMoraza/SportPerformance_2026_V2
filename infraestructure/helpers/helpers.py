18a07ab2e7650d71bcba81b1516acbc1534c2799
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-17 11:36:06 +0100

    Obtener metricas de un deportista en el ultimo año

--- infraestructure/helpers/helpers.py
+++ infraestructure/helpers/helpers.py
@@ -1,7 +1,15 @@
-from datetime import date
+from datetime import date, timedelta
+
 
 def getFechas(year: int):
     fecha_inicio = date(year, 1, 1)
     fecha_fin = date(year + 1, 1, 1)
     return fecha_inicio, fecha_fin
+
+
+def getLastYear(date_current: date):
+    fecha_fin = date_current
+    fecha_inicio = date_current - timedelta(days=365)
+    return fecha_inicio, fecha_fin
+
 
